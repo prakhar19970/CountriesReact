f8531fb7f9cffc80576e00671057526e8b1b8934
prakhar19970 <>
2020-06-05 23:31:56 +0530

    css-updated-region-dropdown populated

---
+++ src/components/searchedCountry.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Searchedcountries extends Component {
+ 
+    render(){
+        return(
+            this.props.searchResults.map((data, index) =>
+            ( 
+                <div className={(data.length < 4 ? 'd-flex new-country-row' :'d-flex country-row' )}>
+                    { data.map((cardData,index)=> (
+                    <div id ={index} className={(data.length < 4 ? 'new-country-block' :"country-block" )}>
+                        <div className="country-flag-div"><img src={cardData.flag} alt="country flag"/></div>
+                        <div className="country-details-div">
+                        <div className="country-name-title">
+                        <h6><b>{cardData.name}</b></h6>
+                        </div>
+                        <div className="country-summary-div">
+                        <div><b>Population:</b> {cardData.population}</div>
+                        <div><b>Region:</b> {cardData.region}</div>
+                        <div><b>Capital:</b> {cardData.capital}</div>
+                        </div>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            ))
+        )}
+}
+export default Searchedcountries;
--- src/components/showCountries.js
+++ src/components/showCountries.js
@@ -1,6 +1,9 @@
 
 import React, { Component } from 'react';
+import SearchBar from "./searchBar";
 import { render } from '@testing-library/react';
+import Allcountries from './showAllCountries';
+import Searchedcountries from './searchedCountry';
 
 // import { Link } from 'react-router-dom'
 
@@ -16,62 +19,45 @@ class Showcountries extends Component {
         searchInput:""
     }
     renderDiv=()=>{
-        console.log('its here ');
         let renderThis;
         const checkValue = this.state.searchInput;
        if(checkValue.length){
-        //    console.log(checkValue.length,"inside if")
-           renderThis= <div></div>
-          const filtercountry= this.state.countriesData.filter(country=>
-            {return country.name.toLowerCase().includes(checkValue.toLowerCase())}) 
-           console.log(filtercountry);  
+            // console.log(checkValue.length,"inside if")
+        const filtercountry = this.state.countriesData.filter(country=>
+            {
+                return country.name.toLowerCase().includes(checkValue.toLowerCase())}) 
+            //  console.log("---------->",this.reStructureData(filtercountry)); 
+
+           renderThis= <Searchedcountries searchResults={this.reStructureData(filtercountry)}/>
        }
        else{
-           renderThis= 
-           this.state.countriesStructureDataFrame.map((data, index) =>
-           (
-              
-          <div className='d-flex country-row'>
-              { data.map((cardData,index)=> (
-                  <div id ={index} className="country-block">
-                  <div className="country-flag-div"><img src={cardData.flag} alt="country flag"/></div>
-                  <div className="country-details-div">
-                  <div className="country-name-title">
-                  <h6><b>{cardData.name}</b></h6>
-                  </div>
-                  <div className="country-summary-div">
-                  <div><b>Population:</b> {cardData.population}</div>
-                  <div><b>Region:</b> {cardData.region}</div>
-                  <div><b>Capital:</b> {cardData.capital}</div>
-                  </div>
-                  </div>
-              </div>
-              ))}
-          </div>
-       ))}
+           renderThis= <Allcountries countriesStructureDataFrame={this.state.countriesStructureDataFrame} />
+          }
     return renderThis;
     }
     searchCountry=(event)=>{
-// console.log(event.target.value);
+    // console.log(event.target.value);
         this.setState({searchInput:event.target.value})
     }
 
     reStructureData=(allcountriesData)=>{
+        // console.log('allcountry----->',allcountriesData);
         let finalData=[];
         let countryRow=[];
         for(let country=0;country<allcountriesData.length;country++)
         {
-            // console.log(res[i]);
-            if((country+1)%5 !== 0)
-            {
-                countryRow.push(allcountriesData[country]);
-            }
-            else
+            //console.log(countryRow);
+            if(countryRow.length === 4)
             {
                 finalData.push(countryRow);
                 countryRow=[];
-            }  
+            }
+            countryRow.push(allcountriesData[country]);
         }
+        if(countryRow.length){
+            finalData.push(countryRow);
+        }
+        // console.log("---finaldata--->",finalData);
         return finalData;
     }
 
@@ -95,24 +81,10 @@ render(){
 
     return(
         <div className="countries-outer-area">
-            <div className="d-flex search-wrapper" >
-                {/* <searchFeild/> */}
-                <input type="text" name="search" className="countries-search-bar" onChange={this.searchCountry}/>
-                <div className="dropdown">
-                <button className="btn white-btn dropdown-toggle" type="button" data-toggle="dropdown">Region
-                <span className="caret"></span></button>
-                  <ul className="dropdown-menu">
-                </ul>
-                </div> 
-                {/* <filterDropdown/> */}
-            </div>
-      {/* <div >
-           {  */}
+           <SearchBar searchFunction={this.searchCountry} filterRegion={this.state.countriesData} />
             <div className="country-blocks-wrapper">
                 {this.renderDiv()}
             </div>
-        {/* //  }
-        // </div> */}
         </div>
         )
          
