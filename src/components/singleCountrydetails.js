c9eef5f0cff714ecc6ec643b48283c8604424556
prakhar19970 <>
2020-06-06 12:56:19 +0530

    added single country page

--- src/components/singleCountrydetails.js
+++ src/components/singleCountrydetails.js
@@ -21,7 +21,7 @@ class SingleCountry extends Component{
                 return data.json();
             }
         }).then(res => {
-            // console.log(res);
+            console.log(res);
             // return res;
              this.setState({ singleCountryData: res})
         });  
@@ -29,6 +29,36 @@ class SingleCountry extends Component{
     render(){
         return(<div className="countries-outer-area">
         <button className="btn white-btn back-btn"><Link to='/'style={{ textDecoration: 'none', color:'hsl(200, 15%, 8%)'}}>Back</Link></button>
+        <div className="d-flex">
+            {console.log(this.state.singleCountryData)}
+            <div>
+                <img src={this.state.singleCountryData.flag} alt="country flag"/>
+            </div>
+            <div className="country-details-div">
+            <div className="country-name-title">
+                <h3><b>{this.state.singleCountryData.name}</b></h3>
+                <div className="d-flex country-summary-div">
+                    <div>
+                <div><b>Native Name:</b> {this.state.singleCountryData.nativeName}</div>
+                <div><b>Population:</b> {this.state.singleCountryData.population}</div>
+                <div><b>Region:</b> {this.state.singleCountryData.region}</div>
+                <div><b>Sub Region:</b> {this.state.singleCountryData.subregion}</div>
+                <div><b>Capital:</b> {this.state.singleCountryData.capital}</div>
+                </div>
+                <div>
+                    {/* <div><b>Top Level Domain:</b> {this.state.singleCountryData.topLevelDomain[0]}</div> */}
+                    {/* <div><b>Currencies:</b> {this.state.singleCountryData.currencies[0].name}</div> */}
+                    {/* <div><b>Languages:</b> {
+                        this.state.singleCountryData.languages.map((lang,index)=>(
+                        <span>{lang.name}</span>
+                        )
+                        )
+                    }</div> */}
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
         </div>)
     }
 }
