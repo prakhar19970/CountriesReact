874a89690ae73c9799c26cb55f1b41f460a30737
prakhar19970 <>
2020-06-07 14:02:13 +0530

    Updates

---
+++ precache-manifest.c973020a916cd0ee098b6179fa50b8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b26cb17a9f471260103c2d91740a29",
+    "url": "/CountriesReact/index.html"
+  },
+  {
+    "revision": "a79ed666a945c54dc05e",
+    "url": "/CountriesReact/static/css/main.2bf13070.chunk.css"
+  },
+  {
+    "revision": "95ad1480996fd6082efe",
+    "url": "/CountriesReact/static/js/2.50a0c974.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/CountriesReact/static/js/2.50a0c974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79ed666a945c54dc05e",
+    "url": "/CountriesReact/static/js/main.fdb3d536.chunk.js"
+  },
+  {
+    "revision": "02d7438dbc5dc2b21c45",
+    "url": "/CountriesReact/static/js/runtime-main.5386ce7c.js"
+  }
+]);
